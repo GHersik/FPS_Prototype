573213dbed0bc8acd58e4d4e7a4e49156c9cb784
GHersik <80617931+GHersik@users.noreply.github.com>
2023-05-23 00:06:09 +0100

    Gravity
    Added gravity to the movement script

--- FPS_Prototype/Assets/Scripts/PlayerMovementController.cs
+++ FPS_Prototype/Assets/Scripts/PlayerMovementController.cs
@@ -6,12 +6,19 @@ public class PlayerMovementController : MonoBehaviour
 {
     [Header("Options")]
     [SerializeField] float speed = 1f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float groundCheckRadius = 0.4f;
+    [SerializeField] LayerMask groundMask;
 
     [Header("References")]
     [SerializeField] InputManager inputManager;
+    [SerializeField] Transform groundCheck;
 
     private CharacterController playerCharacterController;
     private Vector3 movementDirection = Vector3.zero;
+
+    private Vector3 velocity = Vector3.zero;
+    private bool isGrounded;
 
     private void Start()
     {
@@ -21,12 +28,25 @@ public class PlayerMovementController : MonoBehaviour
     private void Update()
     {
         PlayerMove();
+        PlayerFall();
     }
+
+
 
     private void PlayerMove()
     {
         Vector3 move = this.transform.right * movementDirection.x + transform.forward * movementDirection.z;
         playerCharacterController.Move(move * speed * Time.deltaTime);
+    }
+
+    private void PlayerFall()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
+        if (isGrounded)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        playerCharacterController.Move(velocity * Time.deltaTime);
     }
 
     private void UpdateMovementDirection(Vector2 arguments) => movementDirection = new Vector3(arguments.x, 0, arguments.y);
